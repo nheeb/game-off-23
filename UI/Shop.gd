6d86fd704a6eefa318c0979e7318427ee9c931fd
bor <me@bor691.ir>
2023-11-05 19:46:57 +0330

    shop now sets items on fire

--- UI/Shop.gd
+++ UI/Shop.gd
@@ -13,7 +13,8 @@ var scale_in_hand = null
 var shaft_angle : float = 0
 var shaft_angle_time_left : float = 0
 var last_shaft_angle = 0
-
+var fire_duration_left = 0
+var items_in_fire = []
 
 @onready var left_scale_original_pos = get_node('Scale/Left').transform.origin
 @onready var left_scale_connect_original_pos = get_node('Scale/ScaleShaft/left_connect').get_relative_transform_to_parent(get_node('Scale')).origin
@@ -44,6 +45,7 @@ func toggle_item_on_scale(item_ref):
 		item_ref.freeze = true
 		item_ref.on_scale = false
 		item_ref.transform.origin = item_ref.shelf_location
+		item_ref.rotation = 0
 	else:
 		item_ref.freeze = false
 		item_ref.on_scale = true
@@ -56,20 +58,22 @@ func _ready():
 
 func scale_weight():
 	var weight = 0
-	for body in get_node("Scale/Left/scales_paid").get_overlapping_bodies():
+	for body in $Scale/Left/scales_paid.get_overlapping_bodies():
 		if 'price_weight' in body and body != scale_in_hand:
 			weight += body.price_weight
 	return weight
 
 func item_weight():
 	var weight = 0
-	for body in get_node("Scale/Right/scales_paid").get_overlapping_bodies():
+	for body in $Scale/Right/scales_paid.get_overlapping_bodies():
 		if 'price_weight' in body and body != scale_in_hand:
 			weight += body.price_weight
 	return weight
 
+func catch_fire():
+	pass
 
-func _physics_process(delta):
+func handle_scale_dragging(delta):
 	var mousePos = get_local_mouse_position()
 	if Input.is_action_pressed("ShopInteract"):
 		if selected_scale != null:
@@ -96,16 +100,8 @@ func _physics_process(delta):
 			selected_scale = hover[0].collider
 		else:
 			selected_scale = null
-	
-	
-	# debug hover
-	# get_node("Label").text = str(selected_scale)
-	# debug weight
-	get_node("Label").text = str(scale_weight())
 
-	if len(scales) < scale_count_yellow:
-		spawn_yellow_scale()
-	
+func handle_scale_angle(delta):
 	# todo: make logarithmic scale?
 	var shaft_angle_target = max(-max_scale_angle, min(max_scale_angle, (item_weight()-scale_weight())/10))
 	if shaft_angle_target != last_shaft_angle:
@@ -118,3 +114,52 @@ func _physics_process(delta):
 	get_node('Scale/Left').transform.origin = new_pos - left_scale_connect_original_pos + left_scale_original_pos
 	new_pos = get_node('Scale/ScaleShaft/right_connect').get_relative_transform_to_parent(get_node('Scale')).origin
 	get_node('Scale/Right').transform.origin = new_pos - right_scale_connect_original_pos + right_scale_original_pos
+
+func handle_buy_button_visuals():
+	# debug weight
+	get_node("Label").text = str(scale_weight())
+	$Control/Buy.disabled = scale_weight() < item_weight() or item_weight() == 0
+	
+	
+
+func spawn_initial_scales():
+	if len(scales) < scale_count_yellow:
+		spawn_yellow_scale()
+
+func _physics_process(delta):
+	spawn_initial_scales()
+	handle_scale_dragging(delta)
+	handle_scale_angle(delta)
+	handle_buy_button_visuals()
+	handle_fire(delta)
+
+func handle_fire(delta):
+	if fire_duration_left>0:
+		fire_duration_left = max(0, fire_duration_left - delta)
+		if fire_duration_left < 0.4 and len(items_in_fire) > 0:
+			for x in items_in_fire:
+				x.queue_free()
+			items_in_fire = []
+		if fire_duration_left == 0:
+			$Scale/Left/Fire.hide()
+			$Scale/Left/Fire.stop()
+			$Scale/Right/Fire.hide()
+			$Scale/Right/Fire.stop()
+		
+
+func _on_buy_pressed():
+	if scale_weight() >= item_weight():
+		fire_duration_left = 1.5
+		$Scale/Left/Fire.show()
+		$Scale/Left/Fire.play()
+		$Scale/Right/Fire.show()
+		$Scale/Right/Fire.play()
+		# todo: change player owned items
+		for item in get_node("Scale/Right/scales_paid").get_overlapping_bodies():
+			if item is ShopItem:
+				items_in_fire.append(item)
+				print('bought '+item.item_name)
+		for scale in get_node("Scale/Left/scales_paid").get_overlapping_bodies():
+			if scale is DragonScaleItem:
+				items_in_fire.append(scale)
+		catch_fire()
