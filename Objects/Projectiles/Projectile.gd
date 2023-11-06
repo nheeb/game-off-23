dc6d793298da32b61904b3ad00bee325cf997c9e
Sam Holder <samholder@live.co.uk>
2023-11-06 22:47:57 +0000

    ini

--- Objects/Projectiles/Projectile.gd
+++ Objects/Projectiles/Projectile.gd
@@ -3,6 +3,8 @@ class_name Projectile extends Node3D
 @onready var rotation_point = $RotationPoint
 
 var velocity: Vector3 = Vector3.FORWARD
+var oscillation: float = 0
+var decay: float = 1.0
 
 func _ready():
 	look_at(global_position + velocity)
@@ -11,7 +13,13 @@ func _process(delta):
 	rotation_point.rotation.y += delta * PI * 3
 
 func _physics_process(delta):
-	var target_pos = global_position + velocity * delta
+	oscillation += delta * PI
+	decay = max(0, decay - delta * 0.1)
+	if decay < 0.05:
+		queue_free()
+		return
+	
+	var target_pos = global_position + velocity * cos(oscillation) * decay * delta
 	var space_state = get_world_3d().direct_space_state
 	
 	var query = PhysicsRayQueryParameters3D.create(global_position, target_pos)
