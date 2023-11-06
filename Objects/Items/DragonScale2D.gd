8faa13a08a2f5cc36c2ec96d18e1db62fa0973b2
bor <me@bor691.ir>
2023-11-06 15:38:08 +0330

    Shop: Spawn the scales hight out of the camera and with more random y-position

--- Objects/Items/DragonScale2D.gd
+++ Objects/Items/DragonScale2D.gd
@@ -5,18 +5,27 @@ class_name DragonScaleItem extends RigidBody2D
 @export var variant : int = 0: set=set_variant
 const variant_count = 3
 
-@onready var sprite : Sprite2D = get_node("Sprite2D")
+@onready var sprite : Sprite2D = $Sprite
 
 var textures = [
 	load("res://Assets/Sprites/placeholder/dragon_scale_yellow_1.png"),
 	load("res://Assets/Sprites/placeholder/dragon_scale_yellow_2.png"),
 	load("res://Assets/Sprites/placeholder/dragon_scale_yellow_3.png"),
+	load("res://Assets/Sprites/placeholder/dragon_scale_red_1.png"),
+	load("res://Assets/Sprites/placeholder/dragon_scale_red_2.png"),
+	load("res://Assets/Sprites/placeholder/dragon_scale_red_3.png"),
+	load("res://Assets/Sprites/placeholder/dragon_scale_black_1.png"),
+	load("res://Assets/Sprites/placeholder/dragon_scale_black_2.png"),
+	load("res://Assets/Sprites/placeholder/dragon_scale_black_3.png"),
 ]
 
 func set_stage(s):
-	stage=s
+	stage = s
+	var size:float = 1.0+(s-1.0)/2.0
+	$Sprite.scale = Vector2(size, size)
+	$Collision.scale = Vector2(size, size)
 	apply_changes()
-	
+
 func set_variant(v):
 	variant=v
 	apply_changes()
--- UI/Shop.gd
+++ UI/Shop.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 const max_scale_angle = 20
-const scale_spawn_location = Vector2(100, 50)
-const scale_spawn_variation = 10
-@export var scale_count_yellow : int = 2 : set=change_scale_count
+@onready var scale_spawn_location = $scale_spawn_point.transform.origin
+const scale_spawn_variation = 100
+@export var scale_count_yellow : int = 2 : set=set_yellow_scale
+@export var scale_count_red : int = 2 : set=set_red_scale
+@export var scale_count_black : int = 2 : set=set_black_scale
 var scales = []
 var scale_scene = load("res://Objects/Items/DragonScale2D.tscn")
 @onready var scale_container = get_node("DragonScale")
@@ -15,30 +17,41 @@ var shaft_angle_time_left : float = 0
 var last_shaft_angle = 0
 var fire_duration_left = 0
 var items_in_fire = []
+var spawned_scale_count = [0,0,0]
 
 @onready var left_scale_original_pos = get_node('Scale/Left').transform.origin
 @onready var left_scale_connect_original_pos = get_node('Scale/ScaleShaft/left_connect').get_relative_transform_to_parent(get_node('Scale')).origin
 @onready var right_scale_original_pos = get_node('Scale/Right').transform.origin
 @onready var right_scale_connect_original_pos = get_node('Scale/ScaleShaft/right_connect').get_relative_transform_to_parent(get_node('Scale')).origin
 
-func change_scale_count(new_scale_count):
+func set_yellow_scale(new_scale_count):
 	scale_count_yellow = new_scale_count
 	clear_scales()
+func set_red_scale(new_scale_count):
+	scale_count_red = new_scale_count
+	clear_scales()
+func set_black_scale(new_scale_count):
+	scale_count_black = new_scale_count
+	clear_scales()
 
-func spawn_yellow_scale():
+func spawn_scale(stage):
 	var s = scale_scene.instantiate()
-	s.stage = 1
+	s.stage = stage
+	s.price_weight = stage * 10
 	s.variant = rng.randi_range(0, 2)
 	s.transform.origin = scale_spawn_location
 	s.transform.origin.x += rng.randi_range(-scale_spawn_variation, scale_spawn_variation)
+	s.transform.origin.y += rng.randi_range(-scale_spawn_variation, scale_spawn_variation)
 	scales.append(s)
 	scale_container.add_child(s)
+	spawned_scale_count[stage-1] += 1
 
 func clear_scales():
 	if scale_container != null:
 		for child in scale_container.get_children():
 			child.queue_free()
 		scales = []
+		spawned_scale_count = [0,0,0]
 
 func toggle_item_on_scale(item_ref):
 	if item_ref.on_scale:
@@ -119,12 +132,14 @@ func handle_buy_button_visuals():
 	# debug weight
 	get_node("Label").text = str(scale_weight())
 	$Control/Buy.disabled = scale_weight() < item_weight() or item_weight() == 0
-	
-	
 
 func spawn_initial_scales():
-	if len(scales) < scale_count_yellow:
-		spawn_yellow_scale()
+	if spawned_scale_count[0] < scale_count_yellow:
+		spawn_scale(1)
+	if spawned_scale_count[1] < scale_count_red:
+		spawn_scale(2)
+	if spawned_scale_count[2] < scale_count_black:
+		spawn_scale(3)
 
 func _physics_process(delta):
 	spawn_initial_scales()
