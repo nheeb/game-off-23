8b6dd118fed4701e654fd179f006028c168ab5ea
Niels Asselborn <niels.asselborn@stud.uni-due.de>
2023-11-07 15:17:07 +0100

    Add MainMenu and PlayerUI

---
+++ UI/MainMenu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@export var scene_path : String
+@export var ui_path : String
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+
+
+func _on_bt_start_pressed():
+	visible = false
+	get_tree().change_scene_to_file(scene_path)
+	get_tree().root.add_child(load(ui_path).instantiate())
+	
+
