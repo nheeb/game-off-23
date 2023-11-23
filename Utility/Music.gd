dfc5484e83213a4db16b8ac5008eef42774886ab
Niels Asselborn <niels.asselborn@stud.uni-due.de>
2023-11-23 18:01:45 +0100

    Add minor changes to Music.gd

--- Utility/Music.gd
+++ Utility/Music.gd
@@ -2,6 +2,7 @@ extends Node
 
 @export var audio_player : AudioStreamPlayer
 @export var loop : bool = true
+@export var continue_music : bool = true
 
 @export var playlist : Array
 
@@ -16,6 +17,7 @@ var current_track : :
 		current_track = new_track
 		audio_player.stream = current_track
 		audio_player.play()
+
 func _ready():
 	current_track = playlist.pick_random()
 	set_volume(Music.MUSIC_BUS_ID,0.5)
@@ -39,7 +41,7 @@ func _on_audio_stream_player_finished():
 	if (loop):
 		current_track = current_track
 		return
-	else:
+	elif (continue_music):
 		var new_track = playlist.pick_random()
 		if (playlist.size() > 1):
 			while (new_track == current_track):
@@ -59,3 +61,5 @@ func fade_out(duration:float,new_track=null):
 	if (new_track != null):
 		current_track = new_track
 
+func start_track(new_track):
+	current_track = new_track
