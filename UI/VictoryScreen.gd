019464f2bee8d24491e870b143c039e47b06fe0c
Niels Asselborn <niels.asselborn@stud.uni-due.de>
2023-11-28 16:08:04 +0100

    Add placeholders to victory screen

--- UI/VictoryScreen.gd
+++ UI/VictoryScreen.gd
@@ -1,9 +1,11 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	print_debug("Victory")
+	PlayerUI.set_item_visible(false)
+	%AnimationPlayer.play("fly_in")
 
 
-
+func _on_animation_player_animation_finished(anim_name):
+	if (anim_name == "fly_in"):
+		queue_free()
