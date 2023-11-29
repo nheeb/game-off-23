b0514757887003b843aefb5ed5d1e52aa1594102
Sam Holder <samholder@live.co.uk>
2023-11-29 21:25:41 +0000

    Change fire cone to be more dangerous.

--- Objects/Dragon/SpitFireCone.gd
+++ Objects/Dragon/SpitFireCone.gd
@@ -5,6 +5,8 @@ const MAX_DIST = 17.0
 const FIRE_CONE = preload("res://Objects/Projectiles/Firecone.tscn")
 const HINT = preload("res://Objects/Effects/AttackHintFireCone.tscn")
 
+const cone_offset = Vector3.DOWN * 0.1
+
 func get_probability() -> float:
 	return 0.2 if dragon.player_distance >= MIN_DIST and dragon.player_distance <= MAX_DIST else 0.0
 
@@ -17,15 +19,15 @@ func effect_start(index):
 	await dragon.turn_done
 	var hint = HINT.instantiate()
 	Game.world.add_child(hint)
-	hint.global_position = dragon.head_position.global_position
+	hint.global_position = dragon.head_position.global_position + cone_offset
 	hint.global_rotation = dragon.head_position.global_rotation
-	await get_tree().create_timer(.9).timeout
+	await get_tree().create_timer(.6).timeout
 	%AudioDragonHead.stream = firecone_sound.pick_random()
 	%AudioDragonHead.play()
 	dragon.animations.is_spitting_fire = true
 	var fire_cone = FIRE_CONE.instantiate()
 	Game.world.add_child(fire_cone)
-	fire_cone.global_position = dragon.head_position.global_position
+	fire_cone.global_position = dragon.head_position.global_position + cone_offset
 	fire_cone.global_rotation = dragon.head_position.global_rotation
 	fire_cone.hint_can_fade.connect(hint.start_closing)
 	await get_tree().create_timer(1.1).timeout
