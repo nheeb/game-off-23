fba971bee81164048a91cdb07c3af415fb1c66b2
Sam Holder <samholder@live.co.uk>
2023-11-30 23:54:02 +0000

    added tail attack animation.

--- Objects/Dragon/DragonAnimations.gd
+++ Objects/Dragon/DragonAnimations.gd
@@ -6,6 +6,7 @@ class_name DragonAnimations extends Node
 @export var is_dead = false
 @export var is_biting = false
 @export var is_spinning = false
+@export var is_tail_swinging = false
 
 func reset():
 	is_roaring = false
--- Objects/Dragon/TailSwing.gd
+++ Objects/Dragon/TailSwing.gd
@@ -5,17 +5,20 @@ extends DragonState
 
 func get_probability() -> float:
 	if dragon.player_distance <= 6.2 and dragon.player_face_angle > 120.0:
-		return .4
+			return .4
 	else:
-		return 0.0
+			return 0.0
 
 func effect_start(index):
 	dragon.animations.is_flying = false
-	await get_tree().create_timer(0.8).timeout
+	await get_tree().create_timer(0.6).timeout
+	dragon.animations.is_tail_swinging = true
+	await get_tree().create_timer(0.2).timeout
 	dragon.tail_area.activate()
 	%AudioDragonBody.stream = sound_impact.pick_random()
 	%AudioDragonBody.play()
 	await get_tree().create_timer(2.2).timeout
+	dragon.animations.is_tail_swinging = false
 	if is_active():
 		next_state = "Idle"
 
